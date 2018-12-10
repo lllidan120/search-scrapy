89bba60c616c20933f54488d6f6e97195d3357a4
lllidan120 <lllidan120@163.com>
2018-12-10 11:19:48 +0800

    'firtst-commit'

---
+++ AriticleSpider/spiders/jobbole.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+import re
+import scrapy
+from scrapy.http import Request
+from urllib import parse
+
+from AriticleSpider.items import JobBoleArticleItem
+
+class JobboleSpider(scrapy.Spider):
+    name = 'jobbole'
+    allowed_domains = ['blog.jobbole.com']
+    start_urls = ['http://blog.jobbole.com/all-posts/']
+
+    def parse(self, response):
+        """
+        1获取文章列表中的文章url并交给scrapy进行下载交解析
+        2获取下一页的Url
+        """
+        # 解析列表中所有的文章Url
+        post_nodes = response.css("#archive .floated-thumb .post-thumb a")
+        for post_node in post_nodes:
+            image_url = post_node.css("img::attr(src)").extract_first("")
+            post_url = post_node.css("::attr(href)").extract_first("")
+            yield Request(url = parse.urljoin(response.url , post_url) , meta = {"front_image_url": image_url} , callback = self.prase_detail) 
+        # 提取下一页并交给scrapy下载
+        next_urls = response.css(".next.page-numbers::attr(href)").extract_first("")
+        if next_urls:
+            yield Request(url = parse.urljoin(response.url , next_urls) , callback = self.parse) 
+
+    def prase_detail(self , response):
+        article_item = JobBoleArticleItem()
+
+        front_image_url = response.mate.get("front_image_url","")
+        title = response.xpath('//*[@class="entry-header"]/h1/text()').extract()[0]
+        create_date = response.xpath("//p[@class='entry-meta-hide-on-mobile']/text()").extract()[0].strip().replace(" ·","")
+        praise_nums = response.xpath("//span[contains(@class , 'vote-post-up')]/h10/text()").extract()[0]
+        fav_nums = response.xpath("//span[contains(@class , 'bookmark-btn')]/text()").extract()[0]
+        match_re = re.match(".*(\d+).*" , fav_nums)
+        if match_re:
+            fav_nums = int(match_re.group(1))
+        else:
+            fav_nums = 0
+        comment_nums = response.xpath("//a[@href='#article-comment']/span").extract()[0]
+        match_re = re.match(".*(\d+).*" , comment_nums)
+        if match_re:
+            comment_nums = int(match_re.group(1))
+        else:
+            comment_nums = 0
+        content = response.xpath("//div[@class='entry']").extract()[0]
+
+        tag_list = response.xpath("//p[@class='entry-meta-hide-on-mobile']/a/text()").extract()
+        tag_list = [element for element in tag_list if not element.strip().endswith('评论')]
+        tags = ",".join(tag_list)
+
+        article_item["title"] = title
+        article_item["url"] = response.url
+        article_item["create_date"] = create_date
+        article_item["front_image_url"] = front_image_url
+        article_item["praise_nums"] = praise_nums
+        article_item["fav_nums"] = fav_nums
+        article_item["content"] = content
+        article_item["tags"] = tags
+
+        yield article_item
